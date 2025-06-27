af6393c54cb4db7ae20d645652575571a08c1e58
qwepewp <ywin26998@gmail.com>
2025-06-27 14:33:28 +0500

    Create input.jsx

---
+++ kaspi-ud/src/components/ui/input.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`border border-gray-300 rounded px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
+    />
+  )
+})
+Input.displayName = "Input"
